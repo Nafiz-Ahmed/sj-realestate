8726137751b6b13ddda4daa89695856b0873e027
Nafiz Ahmed <nafizbd2001@gmail.com>
2025-07-07 23:34:54 +0600

    version 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,11 +17,7 @@ export default function RootLayout({ children }) {
         {/* Essential meta tags */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#ffffff" />
-
-        {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta property="og:url" content="Canonical link preview URL"></meta>
 
         {/* Google Verification */}
         <meta
